e218dd882ca82f5cd6864a43bde8c72193ccb3bc
Jacob Dipre <jacob.dipre@student.manchester.ac.uk>
2023-02-06 17:38:49 +0000

    joining and making games inc partial db

--- main.py
+++ main.py
@@ -11,13 +11,45 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'secret'
 app.config['SESSION_TYPE'] = 'filesystem'
+app.app_context().push()
 socketio = SocketIO(app)
 db = SQLAlchemy(app)
 engine = create_engine('sqlite:///database.db', echo=False)
 
+class Game(db.Model):
+    game_name = db.Column(db.String(100), primary_key=True)
+    host_username = db.Column(db.String(100))
+    players_connected = db.relationship('Player', backref='game', lazy='select')
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    position = db.Column(db.Integer)
+    symbol = db.Column(db.Integer)
+    money = db.Column(db.Integer)
+    game_id = db.Column(db.Integer, db.ForeignKey('game.game_name'))
+    account_id = db.Column(db.Integer, db.ForeignKey('account.username'))
+
+class Account(db.Model):
+    username = db.Column(db.String(100), primary_key=True)
+#    password = db.Column(db.String(100), nullable=False)
+    total_played = db.Column(db.Integer)
+    games_won = db.Column(db.Integer)
+    game_instances = db.relationship('Player', backref='account', lazy='select')
+
+#admin1 = Account(username='jacob')
+#db.session.add(admin1)
+#db.session.add(Game(game_name='wooga'))
+db.create_all()
+db.session.commit()
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    games = Game.query.all()
+    game_names = []
+    for i in games:
+        print(i.game_name)
+        game_names.append(i.game_name)
+    return render_template('index.html', games = game_names)
 
 '''
 @socketio.on('my event')
@@ -45,35 +77,55 @@ def test_disconnect():
 def game_room():
     if(request.method=='POST'):
         username = request.form['username']
-        room = request.form['room']
+        choice = request.form['choice'] #if the made a game or the name of the game they joined
+        account = Account.query.filter_by(username=username).first()
+        if not account: #player doesnt have account
+            account = Account(username=username)
+            db.session.add(account)
+        player = Player(position=0)
+        db.session.add(player)
+        account.game_instances.append(player) #links account with the player in the new game
+        if choice == 'make': #imaking a game
+            game_name = request.form['game_name']
+            game = Game(game_name=game_name)
+            db.session.add(game)
+        else: #joining game
+            game_name = choice
+            game=Game.query.filter_by(game_name=choice).first()
+        game.players_connected.append(player)#adds player to game    
+        db.session.commit()
         #Store the data in session
         session['username'] = username
-        session['room'] = room
+        session['game_name'] = game_name
         return render_template('game_room.html', session = session)
     else:
         if(session.get('username') is not None):
             return render_template('game_room.html', session = session)
-        else:
-            return redirect(url_for('index'))
+        else: #if not logged in
+            games = Game.query.all()
+            game_names = []
+            for i in games:
+                game_names.append(i.game_name)
+            return redirect(url_for('index'), games = game_names)
 
 @socketio.on('join', namespace='/gameroom')
 def join(message):
-    room = session.get('room')
-    join_room(room)
-    emit('status', {'msg':  session.get('username') + ' has entered the room.'}, room=room)
+    game_name = session.get('game_name')
+    join_room(game_name)
+    emit('status', {'msg':  session.get('username') + ' has entered the room.'}, room=game_name)
 
 @socketio.on('text', namespace='/gameroom')
 def text(message):
-    room = session.get('room')
-    emit('message', {'msg': session.get('username') + ' : ' + message['msg']}, room=room)
+    game_name = session.get('game_name')
+    emit('message', {'msg': session.get('username') + ' : ' + message['msg']}, room=game_name)
 
 @socketio.on('left', namespace='/gameroom')
 def left(message):
-    room = session.get('room')
+    game_name = session.get('game_name')
     username = session.get('username')
-    leave_room(room)
+    leave_room(game_name)
     session.clear()
-    emit('status', {'msg': username + ' has left the room.'}, room=room)
+    emit('status', {'msg': username + ' has left the room.'}, room=game_name)
 
 if __name__ == '__main__':
     socketio.run(app)
