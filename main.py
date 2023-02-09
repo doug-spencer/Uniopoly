ae4c169fb722ba6073e4a1ab221226f0e5ee0c74
n98539ds <n98539ds@manchester.ac.uk>
2023-02-09 16:12:06 +0000

    added validation for sign up and login

--- main.py
+++ main.py
@@ -67,28 +67,43 @@ def check_in_game(game_name, username): #verification fucntion
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
+    def get_account_usernames():
+        accounts = Account.query.all()
+        account_usernames = []
+        for i in accounts:
+            account_usernames.append(i.username)
+        print(account_usernames)
+        return account_usernames
+
     if(request.method=='GET'):
         return render_template('login.html')
         
     elif(request.method=='POST'):
+        account_usernames = get_account_usernames()
         formSubmitted = request.form.get("button")
+        print(formSubmitted)
 
         if formSubmitted == "signup":
-            ##check the name is not in the account table already
             ##render menu once it has been made
             username = request.form.get("signupname")
             
-            new_player = Account(username=username)
-            db.session.add(new_player)
-            db.create_all()
-            db.session.commit()
-
+            if username not in account_usernames:
+                new_player = Account(username=username)
+                db.session.add(new_player)
+                db.create_all()
+                db.session.commit()
+                print("success i think")
+            else:
+                print("account taken")
             return render_template('login.html')
 
         elif formSubmitted == "login":
-            ##add in validation that the username is contained in the account table
-            ##render menu once it has been made
+            #render menu once it has been made
             username = request.form.get("loginname")
+            if username in account_usernames:
+                print("you can log in")
+            else:
+                print("account doesnt exist")
             return render_template('login.html')
     
 
