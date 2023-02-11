4de4b61d2da3b094112c6e9176dbbbb07baca936
Jacob Dipre <jacob.dipre@student.manchester.ac.uk>
2023-02-11 18:42:07 +0000

    pages linked together and further developed lobby

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ engine = create_engine('sqlite:///database.db', echo=False)
 class Game(db.Model):
     game_code = db.Column(db.Integer, primary_key=True)
     index_of_turn = db.Column(db.Integer)
+    game_started = db.Column(db.Boolean)
     #host_id = db.relationship('Player', lazy='select', uselist=False)#use=Flase for one to one 
     players_connected = db.relationship('Player', backref='game', lazy='select')
     #action for specific index
@@ -35,7 +36,7 @@ class Player(db.Model):
     index_in_game = db.Column(db.Integer) #order of players
     symbol = db.Column(db.Integer)
     money = db.Column(db.Integer)
-    game_id = db.Column(db.Integer, db.ForeignKey('game.game_code'))
+    game_code = db.Column(db.Integer, db.ForeignKey('game.game_code'))
     username = db.Column(db.Integer, db.ForeignKey('account.username'))
 
 class Account(db.Model):
@@ -68,7 +69,6 @@ def check_in_game(game_name, username): #verification fucntion
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    
     def get_account_usernames():
         accounts = Account.query.all()
         account_usernames = []
@@ -88,34 +88,40 @@ def index():
         if formSubmitted == "signup":
             ##render menu once it has been made
             username = request.form.get("signupname")
-            
+            session['username'] = username            
             if username not in account_usernames:
                 new_player = Account(username=username)
                 db.session.add(new_player)
                 db.create_all()
                 db.session.commit()
                 print("success i think")
+                return redirect(url_for('menu'))
             else:
                 print("account taken")
-            return render_template('login.html')
+                return render_template('login.html')
 
         elif formSubmitted == "login":
             #render menu once it has been made
             username = request.form.get("loginname")
             if username in account_usernames:
                 print("you can log in")
+                session['username'] = username
+                return redirect(url_for('menu'))
             else:
                 print("account doesnt exist")
-            return render_template('login.html')
+                return render_template('login.html')
 
 @app.route('/menu', methods=['GET', 'POST'])
 def menu():
+    try:
+        username = session['username']
+    except: #player isnt in session (hasnt logged in)
+        return False
     if request.method == 'GET':
         # temp code
         flash("current game codes:")
         for game in Game.query.all():
             flash(game.game_code)
-        #
         return render_template('menu.html')
     formType = request.form.get('button')
     games = Game.query.all()
@@ -124,6 +130,12 @@ def menu():
         if search("^\d{6}$", code):
             for game in games:
                 if game.game_code == int(code):
+                    account = Account.query.filter_by(username=username).first()
+                    player = Player(position=0, index_in_game=len(game.players_connected), money=7)
+                    account.game_instances.append(player)
+                    game.players_connected.append(player)
+                    db.session.add(player)
+                    db.session.commit()                    
                     flash("Game joined!")
                     return render_template('lobby.html')
         flash("Code was not valid")
@@ -139,13 +151,18 @@ def menu():
             for game in games:
                 if game.game_code == int(new_id):
                     unique = False
-        game = Game(game_code=int(new_id), index_of_turn=0)
+        game = Game(game_code=int(new_id), index_of_turn=0, game_started=False)
+        account = Account.query.filter_by(username=username).first()
+        player = Player(position=0, index_in_game=0, money=7)
+        account.game_instances.append(player)
+        game.players_connected.append(player)
+        db.session.add(player)
         db.session.add(game)
         db.session.commit()
         flash("Game created with code " + new_id)
         return render_template('lobby.html')
-                
-    
+
+
 '''
 @socketio.on('my event')
 def test_message(message):
@@ -167,6 +184,31 @@ def test_connect():
 def test_disconnect():
     print('Client disconnected')
 '''
+@app.route('/lobby')
+def lobby():
+    try:
+        username = session['username']
+    except:
+        return False
+    return render_template('lobby.html', session=session)    
+
+@socketio.on('check pregame status', namespace='/lobby') #player updating lobby screen
+def check_pregame_status():
+    try:
+        username = session['username']
+    except:
+        return False
+    player = Player.query(username=username).first()
+    print(player.game_code.index_of_turn)
+    emit('status', {'msg':  session.get('username') + ' has entered the room.'}, room=game_name)
+
+@socketio.on('leave lobby', namespace='/lobby') #player leaving lobby
+def leave_lobby():
+    try:
+        session['username']
+    except:
+        return False
+    emit('status', {'msg':  session.get('username') + ' has entered the room.'}, room=game_name)
 
 @app.route('/gameroom', methods=['GET', 'POST'])
 def game_room():
@@ -182,8 +224,7 @@ def game_room():
         account.game_instances.append(player) #links account with the player in the new game
         if choice == 'make': #imaking a game
             game_name = request.form['game_name']
-
-            game = Game(game_name=game_name, index_of_turn=0)
+            game = Game(game_name=game_name, index_of_turn=0, game_started=False)
             db.session.add(game)
         else: #joining game
             game_name = choice
