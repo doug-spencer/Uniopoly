2c1f3f5282b940a9485cedd2dd6fe59a45d8deed
n98539ds <n98539ds@manchester.ac.uk>
2023-02-16 13:40:19 +0000

    added leave game button functionality

--- main.py
+++ main.py
@@ -137,7 +137,7 @@ def menu():
                     db.session.add(player)
                     db.session.commit()
                     flash("Game joined!")
-                    session['room_code'] = code 
+                    session['game_code'] = code 
                     return redirect(url_for('lobby'))
         flash("Code was not valid")
         return render_template('menu.html')
@@ -160,7 +160,7 @@ def menu():
         db.session.add(player)
         db.session.add(game)
         db.session.commit()
-        session['room_code'] = new_id 
+        session['game_code'] = new_id 
         flash("Game created with code " + new_id)
         return redirect(url_for('lobby'))
 
@@ -192,15 +192,16 @@ def lobby():
         username = session['username']
     except:
         return False
-    room_code = session['room_code']
+    game_code = session['game_code']
 
     if request.method == 'GET':
-        return render_template('lobby.html', room_code=room_code, session=session)
+        return render_template('lobby.html', game_code=game_code, session=session)
 
-    game = Game.query.filter_by(room_code=room_code).fisrt()
+    game = Game.query.filter_by(game_code=game_code).first()
     player = Player.query.filter_by(username=username).first()
     #only runs if POST
     if request.form.get('leaveButton') == 'leave room':
+        Player.query.filter_by(username=username).delete()
         return redirect(url_for('menu'))
     elif request.form.get('startButton') == 'start game':
         if game.players_connected[0].username != username:
@@ -210,7 +211,7 @@ def lobby():
         db.session.commit()
         return redirect(url_for('game_room'))
 
-    return render_template('lobby.html', room_code=room_code, session=session)
+    return render_template('lobby.html', game_code=game_code, session=session)
 
 
        
