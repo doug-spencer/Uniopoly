ec9f31a548bc7c0d3bf7e2430b574433c4b49191
k32726bj <benjamin.james-3@student.manchester.ac.uk>
2023-02-19 17:43:19 +0000

    Capitalised

--- main.py
+++ main.py
@@ -64,11 +64,11 @@ class Player(db.Model):
     money = Column(Integer)
     game_code = Column(Integer, db.ForeignKey('game.game_code'))
     username = Column(Integer, db.ForeignKey('account.username'))
-    utilities = db.relationship('utilities', secondary=link_player_utilities, backref='player', lazy='select')
-    properties = db.relationship('property', secondary=link_player_property, backref='player', lazy='select')
-    bus_stop = db.relationship('bus_stop', secondary=link_player_bus_stop, backref='player', lazy='select')
-    student_union = db.relationship('student_union', secondary=link_player_student_union, backref='player', lazy='select')
-    email = db.relationship('email', secondary=link_player_email, backref='player', lazy='select')
+    utilities = db.relationship('Utilities', secondary=link_player_utilities, backref='player', lazy='select')
+    properties = db.relationship('Property', secondary=link_player_property, backref='player', lazy='select')
+    bus_stop = db.relationship('Bus_stop', secondary=link_player_bus_stop, backref='player', lazy='select')
+    student_union = db.relationship('Student_union', secondary=link_player_student_union, backref='player', lazy='select')
+    email = db.relationship('Email', secondary=link_player_email, backref='player', lazy='select')
 
 class Account(db.Model):
     username = db.Column(db.String(100), primary_key=True)
