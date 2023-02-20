b93225b2fa92781daab6353e96ec1161d9f833a9
busebusee27 <busebusee27@gmail.com>
2023-02-20 06:29:14 +0000

    init files

--- App/__init__.py
+++ App/__init__.py
@@ -16,7 +16,7 @@ socketio = SocketIO(app, cors_allowed_origins='*')
 db = SQLAlchemy(app)
 engine = create_engine('sqlite:///database.db', echo=False)
 
-#global session_id
-#session_id = {}
+db.create_all()
+db.session.commit()
 
-from App import view
+from App.routes import index, lobby, menu, rest
--- App/models/classes/in_game/__init__.py
+++ App/models/classes/in_game/__init__.py
@@ -1,1 +1,5 @@
-import Bus_stop, Email, Property, Student_union, Utilities
+from Bus_stop import Bus_stop
+from Email import Email
+from Property import Property
+from Student_union import Student_union
+from Utilities import Utilities
