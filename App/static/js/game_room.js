e31a4ef655b2ec043296b37de7585cdc7b7dc927
Jacob Dipre <jacob.dipre@student.manchester.ac.uk>
2023-03-09 01:03:10 +0000

    tabs on a popup options box

--- App/static/js/game_room.js
+++ App/static/js/game_room.js
@@ -66,6 +66,32 @@ function buy_property() {
     const property = document.getElementById('property').value;
     socket.emit('buy property', {property: property});
 }
+function close_options() {
+    document.getElementById("options").style.display = "none";
+  }
+function open_options() {
+    document.getElementById("options").style.display = "block";
+  }
+function change_tab(evt, tab_name) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(tab_name).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
 //calls function every 2.5seconds
 setInterval(function() {
     socket.emit('update turn');
